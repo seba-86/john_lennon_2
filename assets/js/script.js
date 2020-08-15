a7225119ee88db24adb3c391ff17c3ee11d8358d
Sebastian Venegas <sebastian@verdefresco.cl>
2020-08-15 03:55:36 -0400

    clear_img

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+$('.carousel').carousel({
+  interval: 7000
+})
+
+$(function () {
+  $(".nav-link ").click(function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var scroll = this.hash;
+
+      $("html, body").animate({
+        scrollTop: $(scroll).offset().top
+      },
+        900, function () {
+
+          location.hash = scroll;
+
+        });
+    }
+  });
+  
+});
+
